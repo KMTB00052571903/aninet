67b6f72015eb8e5b92876dbd82383776cc13c03b
Kami Takegami <1006108156@u.icesi.edu.co>
2025-04-10 00:05:29 -0500

    Added MediaCard.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import HeaderComponent from "./Components/header.js";
 import SignupForm from "./Components/SignupForm.js";
 import LoginForm from "./Components/LoginForm.js";
 import ContentView from "./Components/ContentView.js";
+import MediaCard from "./Components/MediaCard.js";
 
 customElements.define("header-component", HeaderComponent);
 customElements.define("signup-form", SignupForm);
 customElements.define("login-form", LoginForm); 
-customElements.define("content-View", ContentView);   
+customElements.define("content-view", ContentView);
+customElements.define("media-card", MediaCard);
