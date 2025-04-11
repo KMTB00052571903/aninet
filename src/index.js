198a968d514801ae52d26fb97224f335ae23c3a8
rmcarvajal <darwyn089@gmail.com>
2025-04-11 08:05:20 -0500

    merged js elements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import HeaderComponent from "./Components/header.js";
+
+
+
+import HeaderComponent from './Components/Header.js';
+import FooterComponent from './Components/Footer.js';
+import PostList from './Components/postlist.js';
 import SignupForm from "./Components/SignupForm.js";
 import LoginForm from "./Components/LoginForm.js";
 
-customElements.define("header-component", HeaderComponent);
+customElements.define('header-component', HeaderComponent);
+customElements.define('footer-component', FooterComponent);
+customElements.define('post-list', PostList);;
 customElements.define("signup-form", SignupForm);
 customElements.define("login-form", LoginForm);    
