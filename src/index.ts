6c6d63f71214f85855661e402bd7373e00488d5d
Zaphkiel2200 <135277854+Zaphkiel2200@users.noreply.github.com>
2025-05-20 10:40:06 -0500

    Fix Order

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 
 import HeaderComponent from './Components/header';
 import FooterComponent from './Components/footer';
-import PostList from './Components/postlist';
+import PostList from './Components/Posts/postlist';
 import SignupForm from "./Components/Login/SignupForm";
 import LoginForm from "./Components/Login/LoginForm";
-import PostCreator from './Components/post';
+import PostCreator from './Components/Posts/post';
 
 customElements.define("header-component", HeaderComponent)
 customElements.define('footer-component', FooterComponent);
