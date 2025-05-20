f6fe6721d8847b9e76a44eefbba340ee89dd8cf5
Zaphkiel2200 <135277854+Zaphkiel2200@users.noreply.github.com>
2025-05-20 09:41:37 -0500

    Update main header

--- src/Components/header.ts
+++ src/Components/header.ts
@@ -16,22 +16,24 @@ class HeaderComponent extends HTMLElement {
   
     render() {
       this.shadowRoot!.innerHTML = `
-        <style>
-          #header {
+      <style>
+        #header {
             width: 100%;
             display: flex;
             flex-direction: row;
             align-items: center;
             justify-content: space-evenly;
-            padding: 20px;
+            padding: 35px;
             padding-left: 150px;
             padding-right: 150px;
-            margin-left: -40px;
+            margin-left: -100px;
             background: linear-gradient(#111111, transparent)
-          }
+        }
+        
           img {
             width: 335px;
           }
+        
           #navbar {
             display: flex;
             align-items: center;
@@ -39,49 +41,50 @@ class HeaderComponent extends HTMLElement {
             width: 810px;
             gap: 48px;
           }
-          links {
-            display: flex;
-            justify-content: space-evenly;
-            gap: 40px;
-          }
-          p {
-            font-size: 24px;
-            color: white;
-          }
-          input {
-            font-size: 24px;
-            color: white;
-            border: 2px solid #FF0808;
-            border-radius: 40px;
-            padding: 8px;
-            background-color: transparent;
-          }
-          input::placeholder {
-            font-size: 24px;
-            color: #A4A4A4;
-            padding: 8px;
-          }
+        
+        .nav-link {
+          font-size: 18px;
+          color: white;
+          cursor: pointer;
+          margin: 0;
+          text-transform: uppercase;
+          font-weight: bold;
+          letter-spacing: 1px;
+          position: relative;
+        }
+        
+        .nav-link:hover {
+          color: #FF0808;
+        }
+        
+        .nav-link.active {
+          color: #FF0808;
+        }
+        
+        .nav-link.active::after {
+          content: '';
+          position: absolute;
+          bottom: -5px;
+          left: 0;
+          width: 100%;
+          height: 2px;
+          background-color: #FF0808;
+        }
+      </style>
 
-        </style>
-  
-        <header id="header">
-          <logo id="logo">
-            <img src="https://i.ibb.co/PG67j7TQ/logo-medium-white.png" alt="Aninet">
-          </logo>
-          <navbar id="navbar">
-            <links>
-              <p id="home">Home</p>
-              <p id="watch">Watch</p>
-              <p id="category">Category</p>
-              <p id="profile">Profile</p>
-            </links>
-            <searchbar>
-              <input type="text" id="searchbar" placeholder="Search in site">
-            </searchbar>
-          </navbar>
-        </header>
-      `;
-    }
+      <header id="header">
+        <logo id="logo">
+          <img src="https://i.ibb.co/PG67j7TQ/logo-medium-white.png" alt="Aninet">
+        </logo>
+        <navbar id="navbar">
+          <p class="nav-link" data-route="home">Home</p>
+          <p class="nav-link" data-route="watch">Watch</p>
+          <p class="nav-link" data-route="categories">Categories</p>
+          <p class="nav-link" data-route="profile">Profile</p>
+        </navbar>
+      </header>
+    `;
+  }
   }
   
   export default HeaderComponent;
