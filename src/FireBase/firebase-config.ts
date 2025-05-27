3128651e38cc7fb085f82465699150036dcc70f2
Kami Takegami <1006108156@u.icesi.edu.co>
2025-05-27 11:32:15 -0500

    Firebase

--- src/FireBase/firebase-config.ts
+++ src/FireBase/firebase-config.ts
@@ -1,10 +1,9 @@
 
   const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyDhgpeCNHeAoLv5dwGoIXGKT-EW5IssZtM",
+  authDomain: "aninet-a8d5b.firebaseapp.com",
+  projectId: "aninet-a8d5b",
+  storageBucket: "aninet-a8d5b.firebasestorage.app",
+  messagingSenderId: "889720773521",
+  appId: "1:889720773521:web:099812ba985ac9ca7a5830",
   };
